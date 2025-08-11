0a2d4b3bcf57beb5ccc9ebeb9e954e5e9fd4b3e4
frncsdrk <git@frncsdrk.me>
2025-08-11 13:42:11 +0200

    feat: Add support for alertmanager API v2

--- main.go
+++ main.go
@@ -32,27 +32,45 @@ func main() {
 
 	client := &http.Client{Timeout: 5 * time.Second}
 
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("ok"))
+	})
 	// Run health endpoint in background.
-	go func() {
-		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
-			_, _ = w.Write([]byte("ok"))
-		})
-		_ = http.ListenAndServe(":"+httpPort, nil)
-	}()
+	go http.ListenAndServe(":"+httpPort, nil)
 
-	// Run pull and push in background.
-	go func() {
-		for {
-			if err := pullAndPush(client, src, dst, alertmanagerApiVersion); err != nil {
-				log.Printf("pull/push failed: %v", err)
-			}
-			time.Sleep(interval)
+	// Run pull and push
+	for {
+		if err := pullAndPush(client, src, dst, alertmanagerApiVersion); err != nil {
+			log.Printf("pull/push failed: %v", err)
 		}
-	}()
+		time.Sleep(interval)
+	}
+}
 
-	// Keep the process running indefinetly.
-	// s: https://stackoverflow.com/a/36419222
-	select {}
+// AlertV1 represents a single alert returned by /api/v1/alerts
+type AlertV1 struct {
+	Annotations  map[string]string `json:"annotations,omitempty"`
+	Labels       map[string]string `json:"labels"`
+	StartsAt     time.Time         `json:"startsAt"`
+	EndsAt       time.Time         `json:"endsAt"`
+	GeneratorURL string            `json:"generatorURL,omitempty"`
+}
+
+// AlertV2 represents a single alert returned by /api/v2/alerts
+type AlertV2 struct {
+	Annotations  map[string]string `json:"annotations"`
+	Labels       map[string]string `json:"labels"`
+	StartsAt     time.Time         `json:"startsAt"`
+	EndsAt       time.Time         `json:"endsAt"`
+	Fingerprint  string            `json:"fingerprint"`
+	GeneratorURL string            `json:"generatorURL"`
+	// Receivers    []string          `json:"receivers"`
+	Status struct {
+		InhibitedBy []string `json:"inhibitedBy"`
+		SilencedBy  []string `json:"silencedBy"`
+		State       string   `json:"state"`
+	} `json:"status"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 // pullAndPush handles the API interactions with the source and destination alertmanager instances.
@@ -71,19 +89,40 @@ func pullAndPush(c *http.Client, src, dst, apiVersion string) error {
 	}
 	defer respSrc.Body.Close()
 
-	var payload struct {
-		Data []map[string]interface{} `json:"data"`
-	}
-	if err := json.NewDecoder(respSrc.Body).Decode(&payload); err != nil {
-		return err
-	}
-	if len(payload.Data) == 0 {
-		log.Println("Nothing to forward. Awaiting next cycle.")
-		return nil // nothing to forward
-	}
+	switch apiVersion {
+	case "v1":
+		var payload struct {
+			Data []AlertV1 `json:"data"`
+		}
+		if err := json.NewDecoder(respSrc.Body).Decode(&payload); err != nil {
+			return err
+		}
+		if len(payload.Data) == 0 {
+			log.Println("Nothing to forward. Awaiting next cycle.")
+			return nil // nothing to forward
+		}
 
-	// push
-	body, _ := json.Marshal(payload.Data)
+		// push
+		body, _ := json.Marshal(payload.Data)
+		return post(c, dst, apiVersion, body)
+	default:
+		var alerts []AlertV2
+		if err := json.NewDecoder(respSrc.Body).Decode(&alerts); err != nil {
+			return err
+		}
+		if len(alerts) == 0 {
+			log.Println("Nothing to forward. Awaiting next cycle.")
+			return nil // nothing to forward
+		}
+
+		// push
+		body, _ := json.Marshal(alerts)
+		return post(c, dst, apiVersion, body)
+	}
+}
+
+// post handles the REST request to send the data to the destination alertmanager.
+func post(c *http.Client, dst, apiVersion string, body []byte) error {
 	reqDst, _ := http.NewRequestWithContext(context.Background(), "POST", dst+"/api/"+apiVersion+"/alerts", bytes.NewReader(body))
 	reqDst.Header.Set("Content-Type", "application/json")
 
